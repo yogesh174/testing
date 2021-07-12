b85db1c1f9642ce68e1997c9dc06f04418d2a653
Surapaneni Yogesh <surapaneniyogesh11@gmail.com>
2021-07-12 14:15:45 +0530

    Add a file check for deploy.json

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,33 +4,36 @@ echo ${arr1[@]}
 total=${#arr1[@]}
 for(( i=0; i<$total; i++ ))
 do
-	type=`jq -r .deployment_type ${arr1[$i]}/deploy.json`
-	if [ $type == react ]
+	if [ -f `${arr1[$i]}/deploy.json` ]
 	then
-		portno=`jq -r .port_number ${arr1[$i]}/deploy.json`
-		servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
-		servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
-		cd ${arr1[$i]}
-		npm i
-		npm run-script build
-		pm2 serve build $portno --spa
-		cd
-	elif [ $type == node ]
-	then
-		filename=`jq -r .file_name ${arr1[$i]}/deploy.json`
-		nportno=`jq -r .port_number ${arr1[$i]}/deploy.json`
-		servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
-		servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
-		cd ${arr1[$i]}
-		npm i
-		pm2 start $filename
-		cd
-	elif [ $type == html ]
-	then
-		servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
-		servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
-	else
-		echo "Not an app"
+		type=`jq -r .deployment_type ${arr1[$i]}/deploy.json`
+		if [ $type == react ]
+		then
+			portno=`jq -r .port_number ${arr1[$i]}/deploy.json`
+			servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+			servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+			cd ${arr1[$i]}
+			npm i
+			npm run-script build
+			pm2 serve build $portno --spa
+			cd
+		elif [ $type == node ]
+		then
+			filename=`jq -r .file_name ${arr1[$i]}/deploy.json`
+			nportno=`jq -r .port_number ${arr1[$i]}/deploy.json`
+			servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+			servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+			cd ${arr1[$i]}
+			npm i
+			pm2 start $filename
+			cd
+		elif [ $type == html ]
+		then
+			servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+			servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+		else
+			echo "Not an app"
+		fi
 	fi
 done
 
